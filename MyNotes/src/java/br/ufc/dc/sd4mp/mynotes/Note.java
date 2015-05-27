35c922079d6c201d9f3b282d2f15d21b1810d690
Victor <VictorABarbosa@users.noreply.github.com>
2015-05-27 12:53:59 -0300

    Create Note.java

---
+++ MyNotes/src/java/br/ufc/dc/sd4mp/mynotes/Note.java
@@ -0,0 +1,44 @@
+package br.ufc.dc.sd4mp.mynotes;
+
+public class Note {
+
+    private int id;
+
+    private String title;
+
+    private String content;
+
+    private String creationTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreationTime(){ return creationTime; }
+
+    public void setCreationTime(String time){ this.creationTime = time; }
+
+    public String toString() {
+        return "(" + id + ") " + title + ": " + content + " : " + creationTime;
+    }
+}
